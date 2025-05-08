2b42489da72405827b4140bfaa756b0af9f12c2d
adianbu <theadithyaanbu@gmail.com>
2025-05-08 23:23:03 +0530

    Added Template and chat apis in the backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -32,10 +32,68 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
-// import express from "express";
-// import { get } from "http";
-// import { getRandomInt } from "./utils";
+const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
+const express_1 = __importDefault(require("express"));
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
+const prompts_1 = require("./prompts");
 dotenv.config();
-console.log(process.env.CLAUDE_API_KEY);
+const anthropic = new sdk_1.default();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { prompt } = req.body;
+    const response = yield anthropic.messages.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: 'claude-3-haiku-20240307',
+        max_tokens: 100,
+        system: "Return either node or react based on what you think this project should be. Return only the word 'node' or 'react'. Do not return anything extra.",
+    });
+    const ans = response.content[0].text;
+    if (ans == 'node') {
+        res.status(200).json({
+            prompts: [prompts_1.BASE_PROMPT, node_1.basePrompt]
+        });
+        return;
+    }
+    if (ans == 'react') {
+        res.status(200).json({
+            prompts: [prompts_1.BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [react_1.basePrompt]
+        });
+        return;
+    }
+    res.status(400).json({ error: "Wrong type of project" });
+    return;
+}));
+app.post("/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const messages = req.body.messages;
+    const response = yield anthropic.messages.create({
+        messages: messages,
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 8000,
+        system: (0, prompts_1.getSystemPrompt)()
+    });
+    console.log(response);
+    res.json({
+        response: (_a = response.content[0]) === null || _a === void 0 ? void 0 : _a.text
+    });
+}));
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,63 @@
 import * as dotenv from "dotenv";
-// import express from "express";
 import Anthropic from "@anthropic-ai/sdk";
+import express from "express";
+import { TextBlock } from "@anthropic-ai/sdk/resources/messages";
+import fs from 'fs';
+import { basePrompt as NodeBasePrompt } from "./defaults/node";
+import { basePrompt as ReactBasePrompt } from "./defaults/react";
+import { getSystemPrompt,BASE_PROMPT } from "./prompts";
+
 
 dotenv.config();
 const anthropic = new Anthropic();
+const app = express();
+app.use(express.json());
 
-async function main() {
-    const msg = await anthropic.messages.create({
-        model: "claude-3-5-sonnet-20241022",
-        max_tokens: 1000,
-        temperature: 0,
-        messages: [{
-            role: "user",
-            content: "Hello Claude!"
-        }]
-      });
-      console.log(msg);
-}
 
-main();
+app.post("/template", async (req, res) => {
+    const { prompt } = req.body;
+    const response = await anthropic.messages.create({
+        messages: [{role: 'user', content: prompt}],
+        model: 'claude-3-haiku-20240307',
+        max_tokens: 100,
+        system: "Return either node or react based on what you think this project should be. Return only the word 'node' or 'react'. Do not return anything extra.",
+    })
 
+    const ans=(response.content[0] as TextBlock).text;
+    if (ans=='node') {
+        res.status(200).json({
+            prompts: [BASE_PROMPT, NodeBasePrompt]
+        });
+        return;
+    }
+    if (ans=='react') {
+        res.status(200).json({
+            prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${ReactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [ReactBasePrompt]
+        });
+        return;
+    }
+    res.status(400).json({error: "Wrong type of project"});
+    return;
+});
+
+
+app.post("/chat", async (req, res) => {
+    const messages = req.body.messages;
+    const response = await anthropic.messages.create({
+        messages: messages,
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 8000,
+        system: getSystemPrompt()
+    })
+
+    console.log(response);
+
+    res.json({
+        response: (response.content[0] as TextBlock)?.text
+    });
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
